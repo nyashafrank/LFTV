aa64b89f7e8ca95727eb6661313c3fd409f0d63e
nyashafrank <42785108+nyashafrank@users.noreply.github.com>
2020-04-01 21:14:20 -0500

    pp

--- CompactElement.java
+++ CompactElement.java
@@ -12,4 +12,15 @@ public class CompactElement {
 
         return "oldValue: " + oldValue + "\t\t\tnewValue: " + newValue;
     }
+
+
+    public CompactElement() {
+
+    }
+    
+    public CompactElement(int oldV, int newV, Transaction t) {
+        this.oldValue = oldV;
+        this.newValue = newV;
+        this.desc = t;
+    }
 }
